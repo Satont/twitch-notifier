98fa96b7ce9f03fe301e220f39a039f433483b29
Satont <satontworldwide@gmail.com>
2019-10-19 10:07:35 +0300

    feat(vk): move to other library

--- src/libs/vk.ts
+++ src/libs/vk.ts
@@ -1,23 +1,26 @@
-import VkBot from 'node-vk-bot-api'
+import { VK } from 'vk-io'
 import { info, error } from '../helpers/logs'
 import { Twitch } from './twitch'
 import { User } from '../models/User'
 import { Channel } from '../models/Channel'
 import { config } from '../helpers/config'
-import { remove } from 'lodash'
+import { remove, chunk } from 'lodash'
 
-const bot = new VkBot({
+const bot = new VK({
   token: config.vk.token
 })
 const twitch = new Twitch(config.twitch.clientId)
 
-bot.on((ctx) => {
-  info(`Upcoming message from: ${ctx.message.user_id}, message: ${ctx.message.text}`)
-})
+bot.updates.on('message', (ctx, next) => {
+  if (ctx.senderId !== -187752469) {
+    info(`Upcoming message from: ${ctx.senderId}, message: ${ctx.text}`)
+  }
 
-bot.command(['!подписка'], async (ctx) => {
-  const [user] = await User.findOrCreate({ where: { id: ctx.message.from_id }, defaults: { follows: [] } })
-  const argument: string = ctx.message.text.split(' ')[1]
+  return next()
+})
+bot.updates.hear(value => (value.startsWith('!подписка')), async (ctx) => {
+  const [user] = await User.findOrCreate({ where: { id: ctx.senderId }, defaults: { follows: [] } })
+  const argument: string = ctx.text.split(' ')[1]
   if (!argument) {
     return ctx.reply('Вы должны указать на кого подписаться.')
   }
@@ -37,12 +40,13 @@ bot.command(['!подписка'], async (ctx) => {
   }
 })
 
-bot.command(['!отписка'], async (ctx) => {
-  const user = await User.findOne({ where: { id: ctx.message.from_id } })
+
+bot.updates.hear(value => (value.startsWith('!отписка')), async (ctx) => {
+  const user = await User.findOne({ where: { id: ctx.senderId } })
   if (!user) {
     return ctx.reply('В данный момент вы ни на кого не подписаны.')
   }
-  const argument: string = ctx.message.text.split(' ')[1]
+  const argument: string = ctx.text.split(' ')[1]
   if (!argument) {
     return ctx.reply('Вы должны указать от кого отписаться.')
   }
@@ -61,8 +65,8 @@ bot.command(['!отписка'], async (ctx) => {
   }
 })
 
-bot.command(['!подписки'], async (ctx) => {
-  const user = await User.findOne({ where: { id: ctx.message.from_id } })
+bot.updates.hear(value => (value.startsWith('!подписки')), async (ctx) => {
+  const user = await User.findOne({ where: { id: ctx.senderId } })
   if (!user || !user.follows.length) {
     return ctx.reply('В данный момент вы ни на кого не подписаны.')
   } else {
@@ -72,15 +76,28 @@ bot.command(['!подписки'], async (ctx) => {
   }
 })
 
-bot.command(['!команды', '!help', '!commands'], ctx => {
-  ctx.reply('На данный момент доступны следующие команды: !подписка, !отписка, !подписки, !команды')
+bot.updates.hear(value => (value.startsWith('!команды')), async (ctx) => {
+  ctx.reply(`
+  На данный момент доступны следующие команды: 
+  !подписка username
+  !отписка username
+  !подписки 
+  !команды
+  `)
 })
 
 export function say(userId: number | number[], message: string, attachment?: string) {
   info(`Send message to ${Array.isArray(userId) ? userId.join(', ') : userId}. message: ${message}`)
-  bot.sendMessage(userId, message, attachment)
+  const targets = Array.isArray(userId) ? userId : [userId]
+  const chunks = chunk(targets, 100)
+
+  for (const chunk of chunks) {
+    bot.api.messages.send({
+      random_id: Math.random() * (1000000000 - 9) + 10,
+      user_ids: chunk,
+      message
+    })
+  }
 } 
 
-bot.startPolling().then(() => {
-  info('VK bot connected.')
-})
+bot.updates.start().then(() => info('VK bot connected.')).catch(console.error)
