d0864452157c4a0afd3f89b1eca05849b7f4a339
Satont <satontworldwide@gmail.com>
2019-10-19 23:12:28 +0300

    fix(checker): upd

--- src/libs/checker.ts
+++ src/libs/checker.ts
@@ -8,22 +8,25 @@ import { Op } from 'sequelize'
 const twitch = new Twitch(config.twitch.clientId)
 
 async function check () {
-  setTimeout(() => check(), 5 * 60 * 1000)
+  setTimeout(() => check(), 5 * 1000)
   const dbChannels = await Channel.findAll()
   const onlineChannels = flattenDeep(await getOnlineStreams(dbChannels.map(o => o.id)))
+
   for (let dbChannel of dbChannels) {
     const channel = onlineChannels.find(o => Number(o.user_id) === dbChannel.id)
-    
-    if (channel && !dbChannel.online) {
-      await dbChannel.update({ online: true }, { where: { id: dbChannel.id }})
+
+    if (channel && !dbChannel.online) { // channel online, do notify
+      await dbChannel.update({ online: true })
       const users = await User.findAll({ 
         where: { follows: { [Op.contains]: [dbChannel.id] } },
         raw: true
       })
       say(users.map(o => o.id), `${channel.user_name} онлайн!`)
-    } else if (!channel && dbChannel.online) {
-      await dbChannel.update({ online: false }, { where: { id: dbChannel.id }})
-    } else await dbChannel.update({ online: false }, { where: { id: dbChannel.id }})
+    } else if (!channel && dbChannel.online) { // if channel offline but online in db, then set channel as offline in db
+      await dbChannel.update({ online: false })
+    } else if (channel && dbChannel.online) { // skip if channel online and online in db
+      continue
+    } else await dbChannel.update({ online: false })
   }
 }
 check()
