28c97e4939927fc4768c7bfd51c3d31d3cbadba1
Satont <satontworldwide@gmail.com>
2019-10-19 07:32:46 +0300

    feat(twitch): create axios instance instead of own function.

--- src/libs/twitch.ts
+++ src/libs/twitch.ts
@@ -9,53 +9,23 @@ export enum Methods {
   DELETE = 'DELETE'
 }
 
-interface TwitchError {
-  status: string,
-  message: string,
-}
-
-interface Request {
-  method: Methods,
-  endpoint: string,
-  data?: object,
-}
-
 export class Twitch {
-  clientId: string = ''
-  baseUrl: string = 'https://api.twitch.tv/helix/'
+  helix: any;
 
   constructor(clientId: string) {
-    this.clientId = clientId
-  }
-
-  public async request (options: Request) {
-    let query: any;
-    const url: string = `${this.baseUrl}${options.endpoint}`
-    try {
-      query = await axios({
-        method: options.method,
-        url,
-        data: options.data || {},
-        headers: {
-          'Client-ID': this.clientId
-        }
-      })
-      return query.data
-    } catch (e) {
-      const twitchError: boolean = Boolean(e.response.data)
-      const twitchData: TwitchError = e.response.data
-
-      const errorMessage: string = twitchError ? `${twitchData.status} — ${twitchData.message}` : `${e.response.status} — ${e.response.statusText}`
-      error(`Ошибка при запросе ${options.method} ${url}, тело ошибки: ${errorMessage}`)
-      throw new Error('Произошла ошибка при запросе к twitch.')
-    }
+    this.helix = axios.create({
+      baseURL: 'https://api.twitch.tv/helix/',
+      headers: {
+        'Client-ID': clientId
+      },
+    })
   }
 
   public async getChannel(channelName: string): Promise<{id: number, login: string, displayName: string}> {
     try {
-      const request = await this.request({ method: Methods.GET, endpoint: `users?login=${channelName}` })
-      const response = request.data[0]
-      if (!request.data.length) throw new Error(`Канал ${channelName} не найден.`)
+      const request = await this.helix.get(`users?login=${channelName}`)
+      const response = request.data.data[0]
+      if (!request.data.data.length) throw new Error(`Канал ${channelName} не найден.`)
       else return { id: Number(response.id), login: response.login, displayName: response.display_name }
     } catch (e) {
       error(e.message)
@@ -65,8 +35,8 @@ export class Twitch {
 
   public async getChannelsById(channels: number[]): Promise<[{ id: number, displayName: string, login: string }]> {
     try {
-      const request = await this.request({ method: Methods.GET, endpoint: `users?id=${channels.join('&id=')}` })
-      return request.data.map(o => { return { id: Number(o.id), displayName: o.display_name, login: o.login } })
+      const request = await this.helix.get(`users?id=${channels.join('&id=')}`)
+      return request.data.data.map(o => { return { id: Number(o.id), displayName: o.display_name, login: o.login } })
     } catch (e) {
       error(e.message)
       throw new Error(e.message)
@@ -75,8 +45,8 @@ export class Twitch {
 
   public async checkOnline (channels: number[]) {
     try {
-      const request = await this.request({ method: Methods.GET, endpoint: `streams?first=100&user_id=${channels.join('&user_id')}` })
-      return request.data
+      const request = await this.helix.get(`streams?first=100&user_id=${channels.join('&user_id')}`)
+      return request.data.data
     } catch (e) {
       throw new Error(e.message)
     }
