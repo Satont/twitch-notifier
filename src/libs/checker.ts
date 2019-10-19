1dde9d77820499eb5a73a417b187eec66ca37cb2
Satont <satontworldwide@gmail.com>
2019-10-19 12:31:32 +0300

    feat(checker): update checker logic

--- src/libs/checker.ts
+++ src/libs/checker.ts
@@ -1,36 +1,38 @@
 import { Twitch } from './twitch'
 import { Channel } from '../models/Channel'
 import { User } from '../models/User'
-import { chunk, difference } from 'lodash'
+import { chunk, flattenDeep } from 'lodash'
 import { say } from './vk'
 import { config } from '../helpers/config'
 import { Op } from 'sequelize'
 const twitch = new Twitch(config.twitch.clientId)
 
 async function check () {
-  setTimeout(() => check(), 5 * 60 * 1000)
-  const channels = await Channel.findAll({ raw: true })
-  let chunks = chunk(channels, 100)
-  for (let chunk of chunks) {
-    const checkChannels = await twitch.checkOnline(chunk.map(o => Number(o.id)))
-    const offlineChannels = difference(chunk, checkChannels.map(o => o.user_id))
-    for (let channel of offlineChannels) {
-      await Channel.update({ online: false }, { where: { id: channel.id }})
-    }
-    for (let channel of checkChannels) {
-      const dbChannel = await Channel.findOne({ where: { id: Number(channel.user_id) }})
-      if (dbChannel.online) return false
-      else {
-        await Channel.update({ online: true }, { where: { id: dbChannel.id }})
-        const users = await User.findAll({ 
-          where: { 
-            follows: { [Op.contains]: [dbChannel.id] }  
-          },
-          raw: true
-        })
-        say(users.map(o => o.id), `${channel.user_name} онлайн!`)
-      }
-    }
+  setTimeout(() => check(), 10 * 1000)
+  const dbChannels = await Channel.findAll()
+  const onlineChannels = flattenDeep(await getOnlineStreams(dbChannels.map(o => o.id)))
+  for (let dbChannel of dbChannels) {
+    const channel = onlineChannels.find(o => Number(o.user_id) === dbChannel.id)
+    
+    if (channel && !dbChannel.online) {
+      await dbChannel.update({ online: true }, { where: { id: dbChannel.id }})
+      const users = await User.findAll({ 
+        where: { follows: { [Op.contains]: [dbChannel.id] } },
+        raw: true
+      })
+      say(users.map(o => o.id), `${channel.user_name} онлайн!`)
+    } else if (!channel && dbChannel.online) {
+      await dbChannel.update({ online: false }, { where: { id: dbChannel.id }})
+    } else await dbChannel.update({ online: false }, { where: { id: dbChannel.id }})
   }
 }
 check()
+
+async function getOnlineStreams(channels: number[]) {
+  let onlineChannels: any[] = []
+  const chunks = chunk(channels, 100)
+  for (const chunk of chunks) {
+    onlineChannels.push((await twitch.checkOnline(chunk)))
+  }
+  return onlineChannels
+}
