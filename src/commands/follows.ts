6af140ec2f01e807fa6452cfa9ae8bbc41d3000c
Satont <satontworldwide@gmail.com>
2019-12-29 21:40:50 +0300

    fix(commands): remove wrong condition from follows

--- src/commands/follows.ts
+++ src/commands/follows.ts
@@ -6,7 +6,7 @@ const twitch = new Twitch(config.twitch.clientId)
 
 export default async ({ userId, service}: { userId: number, service: 'vk' | 'telegram'}): Promise<boolean | string[]> => {
   const user = await User.findOne({ where: { id: userId, service } })
-  if (!user || !user.follows.length) {
+  if (!user.follows.length) {
     return false
   } else {
     const channels = await twitch.getChannelsById(user.follows)
