e4848eea5abfb56570bd07ca813d2aac7ea2b703
Satont <satontworldwide@gmail.com>
2019-12-29 03:10:28 +0300

    fix(commands): return boolean if user not followed to anyone

--- src/commands/follows.ts
+++ src/commands/follows.ts
@@ -4,10 +4,10 @@ import { User } from '../models/User'
 
 const twitch = new Twitch(config.twitch.clientId)
 
-export default async ({ userId, service}: { userId: number, service: 'vk' | 'telegram'}): Promise<number | string[]> => {
+export default async ({ userId, service}: { userId: number, service: 'vk' | 'telegram'}): Promise<boolean | string[]> => {
   const user = await User.findOne({ where: { id: userId, service } })
   if (!user || !user.follows.length) {
-    return 0
+    return false
   } else {
     const channels = await twitch.getChannelsById(user.follows)
     return channels.map(o => o.displayName)
