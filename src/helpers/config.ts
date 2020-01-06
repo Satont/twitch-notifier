10b099c73ff8186389f322fd584e47ab1a91db46
Satont <satontworldwide@gmail.com>
2020-01-06 03:06:28 +0300

    feat(app): change config import

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,24 +1,4 @@
-const config = {
-  db: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  },
-  vk: {
-    token: process.env.VKTOKEN
-  },
-  telegram: {
-    token: process.env.TGTOKEN
-  },
-  twitch: {
-    clientId: process.env.TWITCH_CLIENTID
-  },
-  panel: {
-    port: process.env.PANEL_PORT || 3000
-  }
-}
+const config = require('../../config.js')
 
 export default { config }
 export { config }
