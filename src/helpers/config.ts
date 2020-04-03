2be340f771391f5134f83340c8ef9037c2396efa
Satont <satontworldwide@gmail.com>
2020-04-03 05:01:24 +0300

    refactor(env): use env variables in config.ts instead of extra require

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,5 +1,16 @@
-const config = require('../../config.js')
-const db = require('../../database.js')[process.env.NODE_ENV]
+export const config = { 
+  vk: {
+    token: process.env.VKTOKEN
+  },
+  telegram: {
+    token: process.env.TGTOKEN
+  },
+  twitch: {
+    clientId: process.env.TWITCH_CLIENTID
+  },
+  panel: {
+    port: Number(process.env.PANEL_PORT) || 3000
+  }
+}
 
-export default { config }
-export { config, db }
+export const db = require('../../database.js')[process.env.NODE_ENV]
