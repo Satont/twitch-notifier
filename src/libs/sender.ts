e35fa4730717981fde681797bffde1d573e29437
Satont <satontworldwide@gmail.com>
2020-04-05 04:34:26 +0300

    refactor(app): change messengers logic, split checker logic and sendmessage logic

---
+++ src/libs/sender.ts
@@ -0,0 +1,39 @@
+import { config } from '../helpers/config'
+import VkService from '../messengers/vk'
+import TelegramService from '../messengers/telegram'
+import { Op } from 'sequelize'
+import { User } from '../models/User'
+import { Twitch, StreamMetadata } from './twitch'
+
+const spaceRegexp = /^\s*$/
+const twitch = new Twitch(config.twitch.clientId)
+
+export const notify = async(streamerName: string, streamerId: number) => {
+  const streamMetaData = await twitch.getStreamMetaData(streamerId)
+  sendVk(streamMetaData)
+  sendTelegram(streamMetaData)
+}
+
+const sendVk = async (streamMetaData: StreamMetadata) => {
+  const photo = await VkService.uploadPhoto(streamMetaData.preview.template.replace('{width}', '1280').replace('{height}', '720'))
+  const game = streamMetaData.game ? `Игра: ${streamMetaData.game}\n` : ''
+  const title = spaceRegexp.test(streamMetaData.channel.status) ? '' : `Название стрима: ${streamMetaData.channel.status}\n`
+  const users = (await User.findAll({ 
+    where: { follows: { [Op.contains]: [Number(streamMetaData.channel._id)] }, service: 'vk' },
+    raw: true
+  })).map(o => o.id)
+  const message = `${streamMetaData.channel.display_name} онлайн!\n${game}${title}https://twitch.tv/${streamMetaData.channel.name}`
+  await VkService.sendMessage({ target: users, message, image: photo.toString() })
+}
+
+const sendTelegram = async (streamMetaData: StreamMetadata) => {
+  const game = streamMetaData.game ? `Game: ${streamMetaData.game}\n` : ''
+  const title = spaceRegexp.test(streamMetaData.channel.status) ? '' : `Title: ${streamMetaData.channel.status}\n`
+  const preview = streamMetaData.preview.template.replace('{width}', '1280').replace('{height}', '720')
+  const users = (await User.findAll({ 
+    where: { follows: { [Op.contains]: [Number(streamMetaData.channel._id)] }, service: 'telegram' },
+    raw: true
+  })).map(o => o.id)
+  const message = `${streamMetaData.channel.display_name} online!\n${game}${title}https://twitch.tv/${streamMetaData.channel.name}`
+  TelegramService.sendMessage({ target: users, message, image: `${preview}?timestamp=${Date.now()}` })
+}
