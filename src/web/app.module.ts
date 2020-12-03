c97e9dcf9e5340f1cfdfe93315e89a88e1079907
Satont <satontworldwide@gmail.com>
2020-12-03 22:37:32 +0300

    upd

--- src/web/app.module.ts
+++ src/web/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { TwitchModule } from './twitch/twitch.module'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [TwitchModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,11 +1,24 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { resolve } from 'path'
 import { AppModule } from './app.module'
+import hbs from 'hbs'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['error', 'warn', 'log'],
   })
+  app.useGlobalPipes(new ValidationPipe())
+  app.useStaticAssets(resolve(process.cwd(), 'public'))
+
+  app.set('view engine', 'hbs')
+  app.set('views', resolve(process.cwd(), 'views'))
+  app.set('view options', {
+    layout: 'layouts/index',
+    templates: resolve(process.cwd(), 'views'),
+  })
+  hbs.registerPartials(resolve(process.cwd(), 'views', 'partials'))
 
   await app.listen(3000, '0.0.0.0')
 }
