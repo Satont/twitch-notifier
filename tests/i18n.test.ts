429fab995412add78bc720b06c38cee8694b2f01
Satont <satontworldwide@gmail.com>
2020-12-09 01:25:57 +0300

    upd

--- tests/i18n.test.ts
+++ tests/i18n.test.ts
@@ -10,17 +10,17 @@ describe('i18n', function() {
   })
 
   it('by default should return "English"', async () => {
-    expect(i18n.translate('language.name')).to.eq('English')
+    expect(i18.translate('language.name')).to.eq('English')
   })
 
   it('clone should switch language to russian', async () => {
-    const newI18n = i18n.clone('ru')
+    const newI18n = i18.clone('ru')
 
     expect(newI18n.lang).to.eq('ru')
   })
 
   it('after clone should return "Русский"', async () => {
-    const newI18n = i18n.clone('ru')
+    const newI18n = i18.clone('ru')
 
     expect(newI18n.translate('language.name')).to.eq('Русский')
   })
