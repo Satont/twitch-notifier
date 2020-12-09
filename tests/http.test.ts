ba927bce0009000a98a680d1cc886910a1cf0580
Satont <satontworldwide@gmail.com>
2020-12-09 13:31:14 +0300

    fix import

--- tests/http.test.ts
+++ tests/http.test.ts
@@ -4,13 +4,13 @@ import { getConnection } from 'typeorm'
 import createDbConnection from './helpers/createDbConnection'
 
 describe('http server should be runned at port 3000', function() {
-  let web: typeof import("/home/satont/Projects/twitch-notifier/src/web/index")
+  let web: typeof import('../src/web')
   let app: NestExpressApplication
 
   before(async () => {
     await createDbConnection()
 
-    web = await import('../src/web/index')
+    web = await import('../src/web')
   })
 
   it('bootstrap web', async () => {
