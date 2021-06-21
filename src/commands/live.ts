c3425c36832e2f28c6fbe190c80b421a4c23470e
Satont <satontworldwide@gmail.com>
2021-06-21 12:26:01 +0300

    Add stream uptime in live command
    fixes #59

--- src/commands/live.ts
+++ src/commands/live.ts
@@ -2,6 +2,9 @@ import { getConnection } from 'typeorm'
 import { Chat } from '../entities/Chat'
 import { Follow } from '../entities/Follow'
 import { I18n } from '../libs/i18n'
+import Twitch from '../libs/twitch'
+import dayjs from 'dayjs'
+import 'dayjs/plugin/relativeTime'
 
 const followRepository = getConnection().getRepository(Follow)
 
@@ -10,15 +13,17 @@ export async function liveCommand({ chat, i18n }: { chat: Chat, i18n: I18n }) {
     return i18n.translate('commands.follows.emptyList')
   }
 
-  const streams = (await followRepository.find({
+  const channels = (await followRepository.find({
     where: { chat },
     relations: ['channel'],
   })).filter(f => f.channel.online).map(f => f.channel)
 
-  if (!streams.length) {
+  const streams = await Twitch.getStreams(channels.map(c => c.id)).catch(() => [])
+
+  if (!channels.length) {
     return i18n.translate('commands.live.empty')
   } else {
-    const names = streams.map(s => `https://twitch.tv/${s.username} | Title: ${s.title} | Category: ${s.category}`)
+    const names = streams.map(s => `https://twitch.tv/${s.userName} | Title: ${s.title} | Category: ${s.gameName} | Uptime: ${dayjs().from(dayjs(s.startDate), true)}`)
     return i18n.translate('commands.live.list', { list: names.join('\n') })
   }
 }
