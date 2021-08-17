547735ce4926fc3e482ce3490ee9871a061c5b0a
Satont <satontworldwide@gmail.com>
2021-08-18 01:49:39 +0300

    Add channel button for unfollow scene
    fixes #51

--- src/services/telegram/scenes/unfollow.ts
+++ src/services/telegram/scenes/unfollow.ts
@@ -1,19 +1,50 @@
-import { BaseScene } from 'telegraf'
+import { BaseScene, Markup } from 'telegraf'
+import { SceneContextMessageUpdate } from 'telegraf/typings/stage'
+import { getRepository } from 'typeorm'
 import telegram from '..'
 import { unFollowCommand } from '../../../commands/unfollow'
+import { Follow } from '../../../entities/Follow'
+import Twitch from '../../../libs/twitch'
+import { chunk } from 'lodash'
 
-export const unFollowScene = new BaseScene('unfollowScene')
+const KEYBOARD_ITEMS_MAX_SIZE = 30
 
-unFollowScene.enter((ctx) => ctx.reply(ctx.i18n.translate('scenes.unfollow.enter')))
-unFollowScene.on('message', async (ctx) => {
-  const result = await unFollowCommand({ chat: ctx.ChatEntity, channelName: ctx.message.text, i18n: ctx.i18n })
-
-  await telegram.sendMessage({
-    target: String(ctx.chat.id),
-    message: result.message,
-  })
-
-  if (result.success) {
-    ctx.scene.leave()
+export interface UnfollowScene extends SceneContextMessageUpdate {
+  state: {
+    follows: Follow[]
+    total: number
+    current: number
   }
-})
+}
+
+export const unFollowScene = new BaseScene<UnfollowScene>('unfollowScene')
+  .enter(async (ctx) => {
+    const follows = await getRepository(Follow).find({ where: { chat: { chatId: String(ctx.chat.id) } }, relations: ['channel'] })
+    const users = await Twitch.getUsers({ ids: follows.map(f => f.channel.id) })
+    const keyboard = Markup.inlineKeyboard(chunk(
+      users.map(u => Markup.callbackButton(u.name, u.name)),
+      KEYBOARD_ITEMS_MAX_SIZE
+    ), 
+    { 
+      columns: 3,
+    })
+
+    ctx.reply(ctx.i18n.translate('scenes.unfollow.enter'), {
+      reply_markup: keyboard,
+    })
+  })
+  .on('message', async (ctx) => {
+    const result = await unFollowCommand({ chat: ctx.ChatEntity, channelName: ctx.message.text, i18n: ctx.i18n })
+
+    await telegram.sendMessage({
+      target: String(ctx.chat.id),
+      message: result.message,
+    })
+
+    if (result.success) {
+      ctx.scene.leave()
+    }
+  })
+  .action(/.+/, (ctx) => {
+    return
+  })
