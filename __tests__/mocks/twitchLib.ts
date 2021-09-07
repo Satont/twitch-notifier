db4faa0d866b130cd43558c5d1044f9539352f01
Satont <satontworldwide@gmail.com>
2021-09-07 08:40:22 +0300

    Add mockObject for twitchLib

--- __tests__/mocks/twitchLib.ts
+++ __tests__/mocks/twitchLib.ts
@@ -1,25 +1,28 @@
 import { createHelixStream } from '../helpers/createHelixStream'
 import { createHelixUser } from '../helpers/createHelixUser'
 
-jest.mock('../../src/libs/twitch', () => ({
-  Twitch: {
-    getUser: jest.fn().mockImplementation((opts) => Promise.resolve(createHelixUser(opts))),
-    getUsers: jest.fn().mockImplementation(({ ids, names }: { ids?: string[], names?: string[] }) => {
-      const type = ids ? 'ids' : 'names'
-      const users = [...(ids || names)].map(arg => {
-        if (type === 'ids') {
-          return createHelixUser({ id: arg })
-        } else {
-          return createHelixUser({ name: arg })
-        }
-      })
-
-      return Promise.resolve(users)
-    }),
-    getStreams: jest.fn().mockImplementation((usersIds: string[]) => {
-      const streams = usersIds.map(id => createHelixStream({ id }))
-
-      return Promise.resolve(streams)
+const twitchObject = {
+  getUser: jest.fn().mockImplementation((opts) => Promise.resolve(createHelixUser(opts))),
+  getUsers: jest.fn().mockImplementation(({ ids, names }: { ids?: string[], names?: string[] }) => {
+    const type = ids ? 'ids' : 'names'
+    const users = [...(ids || names)].map(arg => {
+      if (type === 'ids') {
+        return createHelixUser({ id: arg })
+      } else {
+        return createHelixUser({ name: arg })
+      }
     })
-  }
+
+    return Promise.resolve(users)
+  }),
+  getStreams: jest.fn().mockImplementation((usersIds: string[]) => {
+    const streams = usersIds.map(id => createHelixStream({ id }))
+
+    return Promise.resolve(streams)
+  })
+}
+
+jest.mock('../../src/libs/twitch', () => ({
+  ...twitchObject,
+  Twitch: twitchObject,
 }))
