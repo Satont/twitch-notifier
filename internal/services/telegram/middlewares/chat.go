348412b2e6337edae62221cb1e430acdaad7161b
Satont <satontworldwide@gmail.com>
2023-02-26 12:18:51 +0300

    upd rageywide

--- internal/services/telegram/middlewares/chat.go
+++ internal/services/telegram/middlewares/chat.go
@@ -16,14 +16,14 @@ type ChatMiddleware struct {
 func (c *ChatMiddleware) Wrap(next tgb.Handler) tgb.Handler {
 	return tgb.HandlerFunc(func(ctx context.Context, update *tgb.Update) error {
 		chatId := fmt.Sprintf("%v", update.Chat().ID)
-		user, err := c.Services.Chat.GetByID(chatId, chat.ServiceTelegram)
+		user, err := c.Services.Chat.GetByID(ctx, chatId, chat.ServiceTelegram)
 		if err != nil {
 			zap.L().Error("failed to get chat", zap.Error(err))
 			return nil
 		}
 
 		if user == nil {
-			user, err = c.Services.Chat.Create(chatId, chat.ServiceTelegram)
+			user, err = c.Services.Chat.Create(ctx, chatId, chat.ServiceTelegram)
 			if err != nil {
 				zap.L().Error("failed to create chat", zap.Error(err))
 				return nil
