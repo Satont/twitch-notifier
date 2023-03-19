fba3d9b44281c7fb2e363e2f65b34ddc4a5b4842
Satont <satontworldwide@gmail.com>
2023-03-19 15:47:55 +0000

    upd rageywide

--- pkg/i18n/helpers_test.go
+++ pkg/i18n/helpers_test.go
@@ -17,6 +17,6 @@ func TestGetNested(t *testing.T) {
 	assert.Equal(t, "bar", res, "expected to get a value")
 
 	res, ok = GetNested[string](data, "bar")
-	assert.False(t, ok, "expected to get a value")
-	assert.Equal(t, "", res, "expected to get a value")
+	assert.False(t, ok, "expected to be false")
+	assert.Equal(t, "", res, "expected to not get a value")
 }
