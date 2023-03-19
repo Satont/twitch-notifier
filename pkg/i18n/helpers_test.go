e3d6ced4029b7c1e529a5c172e858b892aa5c214
Satont <satontworldwide@gmail.com>
2023-03-19 15:45:04 +0000

    upd rageywide

---
+++ pkg/i18n/helpers_test.go
@@ -0,0 +1,22 @@
+package i18n
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetNested(t *testing.T) {
+	t.Parallel()
+
+	data := map[string]any{
+		"foo": "bar",
+	}
+
+	res, ok := GetNested[string](data, "foo")
+	assert.True(t, ok, "expected to get a value")
+	assert.Equal(t, "bar", res, "expected to get a value")
+
+	res, ok = GetNested[string](data, "bar")
+	assert.False(t, ok, "expected to get a value")
+	assert.Equal(t, "", res, "expected to get a value")
+}
